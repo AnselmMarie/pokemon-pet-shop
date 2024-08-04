452cedf4441cc0a38c80e331db47d2fa4ba7faa2
Anselm Marie <ansmarie@icloud.com>
2024-08-04 09:29:40 -0400

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/OX614sa9Gf

---
+++ apps/pokemon-pet-shop/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="pokemon-pet-shop" />
+    </div>
+  );
+}
+
+export default App;
