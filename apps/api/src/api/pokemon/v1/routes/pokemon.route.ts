87e330ac51e5e294d1d616eb200f584005e94260
Anselm Marie <ansmarie@icloud.com>
2025-11-26 01:14:38 -0500

    Pokemon pricing is now working

--- apps/api/src/api/pokemon/v1/routes/pokemon.route.ts
+++ apps/api/src/api/pokemon/v1/routes/pokemon.route.ts
@@ -3,11 +3,13 @@ import express from 'express';
 import { getPokemonDetailsController } from '../controllers/pokemon.details.controller';
 import { getPokemonListController } from '../controllers/pokemon.list.controller';
 import { getPokemonSpeciesController } from '../controllers/pokemon.species.controller';
+import { getPricingController } from '../controllers/pricing.controller';
 
 const pokemonRoute: any = express.Router();
 
 pokemonRoute.get('/list', getPokemonListController);
 pokemonRoute.get('/details/:id', getPokemonDetailsController);
 pokemonRoute.get('/species/:id', getPokemonSpeciesController);
+pokemonRoute.get('/pricing', getPricingController);
 
 export { pokemonRoute };
--- libs/services/pokemon-api/src/lib/pricing.ts
+++ libs/services/pokemon-api/src/lib/pricing.ts
@@ -3,7 +3,8 @@ import { useQuery } from '@tanstack/react-query';
 import { axiosInstance } from '@utils/apiClient';
 
 export const useGetPokemonPricing = () => {
-  const queryFn = () => axiosInstance.get<PokemonPricing.Base>('pricing');
+  const queryFn = () =>
+    axiosInstance.get<PokemonPricing.Base>('pokemon/pricing');
 
   return useQuery({
     queryKey: ['pricing'],
