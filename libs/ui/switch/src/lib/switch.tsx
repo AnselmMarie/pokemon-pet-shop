051e4d35f323b9e8224c7c87d51a0896dc8d29fd
Anselm Marie <ansmarie@icloud.com>
2025-12-03 21:36:40 -0500

    cleanup

--- libs/ui/switch/src/lib/switch.tsx
+++ libs/ui/switch/src/lib/switch.tsx
@@ -1,12 +1,9 @@
 import { ReactElement } from 'react';
 
-// import { classNamesUtil, noopUtil } from '@pokemon-pet-shop/utils';
-
 import { Box } from '@ui/box';
 import { Icon } from '@ui/icon';
 
 import { SwitchProps } from './switch.interface';
-// import { styles } from './switch.module';
 import useSwitchLogic from './use.switch.logic';
 
 export const Switch = ({
@@ -26,11 +23,6 @@ export const Switch = ({
   const { getCircleThemeStyle, onInitLeftClick, onInitRightClick } =
     useSwitchLogic(defaultValue, val, onLeftClick, onRightClick);
 
-  // background: var(--theme-netural-200-mode);
-  // padding: var(--theme-spacing-5) var(--theme-spacing-10);
-  // justify-content: space-between;`
-  // box - shadow: inset 0 3px 3px rgb(0 0 0 / 10 %);
-
   return (
     <Box
       className={`flex flex-row bg-lightGrey pl-2xs pr-2xs w-[109px] h-[61px] items-center justify-between relative rounded-pill ${className}`}
@@ -48,8 +40,6 @@ export const Switch = ({
           onClick={onInitLeftClick}
         />
       ) : null}
-      {/* width: 31px;
-      height: 39px; padding: 2px; */}
 
       {iconRight ? (
         <Icon
@@ -64,13 +54,8 @@ export const Switch = ({
           onClick={onInitRightClick}
         />
       ) : null}
-      {/*  background-color:
-      var(--theme-blue-600-mode);  border-radius:
-      var(--theme-radius-circle); transition: left 100ms ease-in-out; */}
-      {/* h-[31px] w-[31px] */}
       <Box
         className={`absolute top-[11px] h-[38px] w-[38px] bg-primary z-0 shadow transition-all duration-300 ease-in-out rounded-circle ${getCircleThemeStyle}`}
-        // className={classNamesUtil(newStyles.iconCircle, getCircleThemeStyle)}
       />
     </Box>
   );
